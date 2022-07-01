b3e6488d58dbe600c61f1d566527f06c73f99135
alejandrogh7 <alejo1garciasosa@gmail.com>
2022-07-01 09:07:49 -0500

    09

--- Ejercicios/09-SpyOn/SpyOn.js
+++ Ejercicios/09-SpyOn/SpyOn.js
@@ -1,6 +1,28 @@
-function spyOn (fn) {
+function spyOn(fn) {
   // Your code here:
 
+  let callCount = 0;
+  let calledWith = new Map();
+  let calledReturn = new Map();
+
+  const spy = (...args) => {
+    for (const arg of args) calledWith.set(arg, true);
+    callCount++;
+    const result = fn(...args);
+    calledReturn.set(result, true);
+    return result;
+  };
+
+  spy.getCallCount = () => callCount;
+
+  spy.wasCalledWith = (value) => {
+    return calledWith.get(value) ? true : false;
+  };
+
+  spy.returned = (value) => {
+    return calledReturn.get(value) ? true : false;
+  };
+  return spy;
 }
 
-module.exports = spyOn
+module.exports = spyOn;
